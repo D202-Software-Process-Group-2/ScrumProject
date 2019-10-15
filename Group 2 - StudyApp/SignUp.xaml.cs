df94ac57667cc0445bc945bb92183a0d9bb2d1df
1711971 <1711971@SW22>
2019-10-15 15:25:27 +1300

    Recreated SignUp Page

--- Group 2 - StudyApp/SignUp.xaml.cs
+++ Group 2 - StudyApp/SignUp.xaml.cs
@@ -23,10 +23,5 @@ namespace Group_2___StudyApp
         {
             InitializeComponent();
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
     }
 }
