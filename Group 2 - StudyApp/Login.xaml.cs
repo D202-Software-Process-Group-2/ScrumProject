d65e2dab9bc26269e46c917ce3cd6365d6b60401
1594862 <1594862@SW23>
2019-10-22 14:50:05 +1300

    coded the signin page

--- Group 2 - StudyApp/Login.xaml.cs
+++ Group 2 - StudyApp/Login.xaml.cs
@@ -11,6 +11,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace Group_2___StudyApp
 {
@@ -22,6 +26,39 @@ namespace Group_2___StudyApp
         public Login()
         {
             InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection con = new SqlConnection(Properties.Settings.Default.DataConString);
+            try
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+                string query = "Select count(1) from Student where Student_Id=@Student_Id and password=@password";
+                SqlCommand sqlCmd = new SqlCommand(query, con);
+                sqlCmd.CommandType = CommandType.Text;
+                sqlCmd.Parameters.AddWithValue("@Student_Id", tbxId.Text);
+                sqlCmd.Parameters.AddWithValue("@password", pbxPass.Password);
+                int count= Convert.ToInt32(sqlCmd.ExecuteScalar());
+                if (count == 1)
+                {
+                    new MainWindow().Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Username or password is incorrect");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
         }
     }
 }
