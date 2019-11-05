3d7f888e4ec2e329ba3400b855e6025de4ea477a
1711971 <1711971@SW22>
2019-11-05 15:05:35 +1300

    Add Paper Function

--- Group 2 - StudyApp/Add.xaml.cs
+++ Group 2 - StudyApp/Add.xaml.cs
@@ -11,6 +11,11 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
+
 
 namespace Group_2___StudyApp
 {
@@ -22,6 +27,35 @@ namespace Group_2___StudyApp
         public Add()
         {
             InitializeComponent();
+        }
+
+        public int compulsory = 0;
+
+        private void Btnaddpaper_Click(object sender, RoutedEventArgs e)
+        {
+            string Papercode = tbxPapercode.Text;
+            string Name = tbxpapername.Text;
+            string Description = tbxpaperdescription.Text;
+            int Year = int.Parse(cbxYear.SelectedItem.ToString());
+            string PreReq = tbxprereq.Text;
+
+            if (chkcompulsory.IsChecked == true)
+            {
+                compulsory = 1;
+            }
+
+
+            string sqlquery = "INSERT INTO Paper(Paper_Code, Name, Description, Year, PreRequisite, Compulsory) " +
+                "Values('" + Papercode + "', '" + Name + "', '" + Description + "', '" + Year + "', '" + PreReq + "', '" + compulsory + "')";
+
+            SqlConnection con = new SqlConnection(Properties.Settings.Default.DataConString);
+            SqlCommand cmd = new SqlCommand(sqlquery, con);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            this.Close();
         }
     }
 }
--- Group 2 - StudyApp/AdminPanel.xaml.cs
+++ Group 2 - StudyApp/AdminPanel.xaml.cs
@@ -51,7 +51,7 @@ namespace Group_2___StudyApp
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            new Add().Show();
         }
     }
 }
